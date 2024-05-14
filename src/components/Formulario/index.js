9eb14830c6bb9f6d1ff056286168192f77474e6e
hyan ventura <hyanventura12@hotmail.com>
2024-05-14 00:17:02 -0300

    feat: added css and make formulario run

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -5,15 +5,6 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'Ux e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ] 
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -57,7 +48,7 @@ const Formulario = (props) => {
                  />
                 <ListaSuspensa 
                     required={true} label='Time' 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
